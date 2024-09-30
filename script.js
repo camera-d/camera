6827dcdca91c81a1f4b3daef436762111c72dfb3
camera-d <kiwamukuro8@gmail.com>
2024-09-30 17:36:59 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1035,6 +1035,11 @@ document.addEventListener("DOMContentLoaded", () => {
   // イベントリスナーの設定
   capture2.addEventListener("click", captureImage);
 
+  //内カメ外カメのトグル
+document.getElementById("toggle-camera2").addEventListener("click", () => {
+  isFrontCamera = !isFrontCamera;
+  startCamera();
+});
   // カメラの起動
   
 });
